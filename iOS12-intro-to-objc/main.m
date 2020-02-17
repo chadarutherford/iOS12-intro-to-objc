8cb098c35695fc532711130ec73d16f2354d2499
Chad Rutherford <chad_rutherford@outlook.com>
2020-02-17 12:13:31 -0500

    Initial Commit

---
+++ iOS12-intro-to-objc/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  iOS12-intro-to-objc
+//
+//  Created by Chad Rutherford on 2/17/20.
+//  Copyright © 2020 Chad Rutherford. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
